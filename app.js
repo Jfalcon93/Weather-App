10f06ab282f4fab9a16076f0ec711fc34e83dbf7
Jordan Falcon <jordanfalcon@Jordans-MacBook-Pro.local>
2019-05-06 01:03:29 -0600

    Pushing all code

---
+++ app.js
@@ -0,0 +1,99 @@
+const temperatureDescription = document.querySelector('.temperature-description');
+const temperatureDegree = document.querySelector('.temperature-degree');
+const locationTimezone = document.querySelector('.location-timezone');
+const temperatureSection = document.querySelector('.temperature');
+const temperatureSpan = document.querySelector('.degree-indicator');
+const extraDetails = document.querySelector('.extra-details');
+const windSpan = document.querySelector('.wind');
+const humiditySpan = document.querySelector('.humidity');
+const dewPointSpan = document.querySelector('.dewPoint');
+const days = document.querySelector('.weekday');
+const dayOne = days.children[0];
+const dayTwo = days.children[1];
+const dayThree = days.children[2];
+const dayFour = days.children[3];
+const dayFive = days.children[4];
+
+window.addEventListener('load',  () => {
+    let long;
+    let lat;
+    
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/b3cea05112a11957b2128a5114114cb6/${lat},${long}?exclude=alerts,flags`
+            
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                const {temperature, summary, icon, windSpeed, humidity, dewPoint} = data.currently;
+                const daily = data.daily;
+                console.log(daily.data[0]);
+                // Set DOM Elements from the API
+                temperatureDegree.textContent = Math.floor(temperature);
+                temperatureDescription.textContent = summary;
+                locationTimezone.textContent = data.timezone;
+                // Formula for celsius
+                let celsius = (temperature - 32) * (5/9);
+                // Set Icon
+                setIcons(icon, document.querySelector('.icon'));
+                // Set Current Wind, Humidity, Dew Point
+                windSpan.textContent = `Wind ${Math.floor(windSpeed)} mph`;
+                humiditySpan.textContent = `Humidity ${humidity * 100}%`;
+                dewPointSpan.textContent = `Dew Point ${Math.floor(dewPoint)}`;
+                //Change temperature to Celsius/Farenheit
+                temperatureSection.addEventListener('click', () => {
+                    if(temperatureSpan.textContent === 'F') {
+                        temperatureSpan.textContent = "C";
+                        temperatureDegree.textContent = Math.floor(celsius);
+                    }
+                    else {
+                        temperatureSpan.textContent = "F";
+                        temperatureDegree.textContent = Math.floor(temperature);
+                    }
+                })
+                
+                dayOne.children[0].textContent = "Monday";
+                setIcons(daily.data[0].icon, document.querySelector('.icon-one'));
+                dayOne.children[2].textContent = `H:${Math.floor(daily.data[0].temperatureHigh)}° L:${Math.floor(daily.data[0].temperatureLow)}°`;
+                dayOne.children[3].textContent = daily.data[0].summary;
+                
+                dayTwo.children[0].textContent = "Tuesday";
+                setIcons(daily.data[1].icon, document.querySelector('.icon-two'));
+                dayTwo.children[2].textContent = `H:${Math.floor(daily.data[1].temperatureHigh)}° L:${Math.floor(daily.data[1].temperatureLow)}°`;
+                dayTwo.children[3].textContent = daily.data[1].summary;
+                
+                dayThree.children[0].textContent = "Tuesday";
+                setIcons(daily.data[2].icon, document.querySelector('.icon-three'));
+                dayThree.children[2].textContent = `H:${Math.floor(daily.data[2].temperatureHigh)}° L:${Math.floor(daily.data[2].temperatureLow)}°`;
+                dayThree.children[3].textContent = daily.data[2].summary;
+                
+                dayFour.children[0].textContent = "Tuesday";
+                setIcons(daily.data[3].icon, document.querySelector('.icon-four'));
+                dayFour.children[2].textContent = `H:${Math.floor(daily.data[3].temperatureHigh)}° L:${Math.floor(daily.data[3].temperatureLow)}°`;
+                dayFour.children[3].textContent = daily.data[3].summary;
+                
+                dayFive.children[0].textContent = "Tuesday";
+                setIcons(daily.data[4].icon, document.querySelector('.icon-five'));
+                dayFive.children[2].textContent = `H:${Math.floor(daily.data[4].temperatureHigh)}° L:${Math.floor(daily.data[4].temperatureLow)}°`;
+                dayFive.children[3].textContent = daily.data[4].summary;
+                
+            })
+        });
+    }
+    else {
+        h1.textContent = "browser does not support";
+    }
+    
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({color: 'white'});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
+});
---
+++ background.js
@@ -0,0 +1,60 @@
+const backgroundColors = {
+    midnight: "linear-gradient(to top, #0f0c29, #302b63, #24243e)",
+    dawn: "linear-gradient(to top, #4BC0C8, #C779D0, #FEAC5E)",
+    morning: "linear-gradient(to top, #fceabb, #f8b500)",
+    afternoon: "linear-gradient(to top, #2980b9, #6dd5fa, #ffffff)",
+    evening: "linear-gradient(to top, #b92b27, #1565c0)"
+};
+
+const timeElement = document.getElementById('time');
+
+function checkTime() {
+    
+    let currentTime = new Date();
+    let hour = currentTime.getHours();
+    let minute = currentTime.getMinutes();
+    let amPM = "AM";
+    
+    if (hour == 0) {
+        hour = 12;
+    }
+    else if (hour > 12) {
+        hour = hour - 12;
+        amPM = "PM";
+    }
+    else {
+        hour = hour;
+    }
+    
+    hour = (hour < 10) ? "0" + hour : hour;
+    minute = (minute < 10) ? "0" + minute : minute;
+    
+    return `${hour}:${minute} ${amPM}`
+}
+
+function setBackground() {
+    
+    let time = new Date();
+    let background = document.getElementById('body');
+    
+    if(time.getHours() >= 22 || time.getHours() <= 5) {
+        background.style.background = backgroundColors['midnight'];
+    }
+    else if (time.getHours() > 5 && time.getHours() <= 9) {
+        background.style.background = backgroundColors['dawn'];
+    }
+    else if (time.getHours() > 9 && time.getHours() <= 12){
+        background.style.background = backgroundColors['morning'];
+    }
+    else if (time.getHours() > 12 && time.getHours() <= 16){
+        background.style.background = backgroundColors['afternoon'];
+    }
+    else if (time.getHours() > 16 && time.getHours() < 22) {
+        background.style.background = backgroundColors['evening'];
+    }    
+}
+
+window.addEventListener('load', () => {
+    setBackground();
+    timeElement.textContent = `Last update was at: ${checkTime()}`;
+})
